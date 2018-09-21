092f39d835a0cea30193a47a0540b2464d48041c
MattJeong <junhj7230@gmail.com>
2018-09-21 14:42:05 +1000

    Set up Start script and herocu port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -32,6 +33,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Sever is runing on localhost 3000');
+app.listen(port, () => {
+  console.log(`Sever is runing on port: ${port}`);
 });
